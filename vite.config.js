56a2edd045f21f422d2a732ccd84c8b7eb50673b
lulliz <romanlullis@gmail.com>
2023-08-21 21:17:03 +0700

    fix PWA

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,28 @@ import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
     vue(),
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      manifest: {
+        name: 'Zoo Clicker',
+        short_name: 'ZooClicker',
+        description: 'Small clicker game where you can feel yourself director of the zoo',
+        theme_color: '#252834',
+        icons: [
+          {
+            src: 'favicon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'favicon.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+     })
   ],
   resolve: {
     alias: {
